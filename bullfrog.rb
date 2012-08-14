5ff9b74398b861b0348522ff98dc6110b34a36bf
Edward Benson <edward.benson@gmail.com>
2012-08-13 23:13:13 -0400

    something

--- bullfrog.rb
+++ bullfrog.rb
@@ -5,6 +5,7 @@ require 'json'
 
 get '/ribbit' do
   content_type :js
+  headers 'Access-Control-Allow-Origin' => '*'  
   url = params[:url]
   id = "#" + params[:id]
   callback = params[:callback]
@@ -13,3 +14,52 @@ get '/ribbit' do
   frag_json = frag.to_json
   "#{callback}(#{frag_json})"
 end
+
+get '/parrot' do
+  content_type :text
+  headers 'Access-Control-Allow-Origin' => '*'  
+  url = params[:url]
+  callback = params[:callback]
+  frag = open(url) { |f| f.read }
+  frag_json = frag.to_json
+  "#{callback}(#{frag_json})"
+end
+
+get '/byschema' do
+  content_type :js
+  headers 'Access-Control-Allow-Origin' => '*'  
+  url = params[:url]
+  callback = params[:callback]
+  frags = []
+  
+  doc = Nokogiri::HTML(open(url))
+  doc.css(".template").each do |node|
+    frags << {:content => node.to_s}
+  end
+  "#{callback}(#{frags.to_json})"
+end
+
+post '/copy' do
+  content_type :js
+  headers 'Access-Control-Allow-Origin' => '*'  
+  content = params[:data]
+  File.open('copied.txt', 'w') { |f| f.write(content) }
+  "''"
+end
+
+get '/paste' do
+  content_type :js
+  headers 'Access-Control-Allow-Origin' => '*'  
+  callback = params[:callback] || "callback"
+  content = ""
+  begin
+    File.open('copied.txt', 'r') { |f| content = f.read }
+    File.delete('copied.txt')
+  rescue
+    content = ''
+  end
+  "#{callback}(#{content.to_json})"
+end
+# try copy paste using the inter-tab version of web sockets
+
+# use example of linking/embedding to academic papers and show it as a reference (so resuse their data, apply my style)
